113e1cc654f64b9ea2620ace0dff01f8f8f63ff4
designed2code <88329785+designed2code@users.noreply.github.com>
2021-08-14 22:11:38 +0530

    Setting attribute value using setAtrribute() method

---
+++ setAttribute.js
@@ -0,0 +1,7 @@
+//Creating and Setting the class of the newly created element and appending it to the body
+
+const myElement = document.createElement('div') //Creates a HTML div
+
+myElement.setAttribute('class', 'dcode') // Sets a class of dcode to the created div element
+
+document.body.appendChild(myElement) // Appends the div with the class of dcode to the body 
