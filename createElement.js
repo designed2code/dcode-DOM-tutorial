ef0e5e455f3643db93922db64a0a5c2b6934f1b2
designed2code <88329785+designed2code@users.noreply.github.com>
2021-08-14 19:57:34 +0530

    Create an input tag and append it to the body

---
+++ createElement.js
@@ -0,0 +1,3 @@
+// Create an input tag and append it to the body
+let newElement = document.createElement('input') //Creates an input tag
+document.body.appendChild(newElement) //Appends the created input element in the body
